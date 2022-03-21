8db37bae09f51d8bdbad5a8b9d81b2370fdb1688
GKxxQAQ <2863746329@qq.com>
2022-03-21 20:51:22 +0800

    update

--- rec5/example/a_plus_b.c
+++ rec5/example/a_plus_b.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main() {
-  FILE *in = fopen("in_file", "r");
+  FILE *in = fopen("testcase.in", "r");
+  FILE *out = fopen("testcase.out", "w");
   int a, b;
-  scanf("%d%d", &a, &b);
-  printf("%d\n", a + b);
+  fscanf(in, "%d%d", &a, &b);
+  fprintf(out, "%d\n", a + b);
+  fclose(in);
+  fclose(out);
   return 0;
 }
